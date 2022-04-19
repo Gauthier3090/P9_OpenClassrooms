969a60370e073b18f033cccdb9ea30a7ec80b957
Gauthier3090 <gauthier.pladet@gmail.com>
2022-04-19 09:46:07 +0200

    rework css

--- src/core/forms.py
+++ src/core/forms.py
@@ -2,15 +2,24 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50, widget= forms.TextInput(attrs={"placeholder": "Nom d'utilisateur"}), label="Nom d'utilisateur")
-    password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={"placeholder": "Mot de passe"}), label="Mot de passe")
+    username = forms.CharField(max_length=50, widget=forms.TextInput(
+        attrs={"placeholder": "Nom d'utilisateur", "class": "input-login"}),
+        label="")
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput(
+        attrs={"placeholder": "Mot de passe", "class": "input-login"}),
+        label="")
+
 
 class SignForm(UserCreationForm):
     password1 = forms.CharField(label=("Password"),
-        widget=forms.PasswordInput(attrs={"placeholder": "Mot de passe"}))
-    password2 = forms.CharField(label=("Password confirmation"),
-        widget=forms.PasswordInput(attrs={"placeholder": "Confirmez votre mot de passe"}))
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Mot de passe"}))
+    password2 = forms.CharField(label=("Password confirmation"), widget=
+    forms.PasswordInput(
+            attrs={"placeholder": "Confirmez votre mot de passe"}))
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username',)
