83c058c38e50a91fa062f021654f68ddd8a83c8e
Gauthier3090 <gauthier.pladet@gmail.com>
2022-05-16 10:45:03 +0200

    create flux

--- src/core/forms.py
+++ src/core/forms.py
@@ -5,11 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50, widget=forms.TextInput(
-        attrs={"placeholder": "Nom d'utilisateur", "class": "form-control w-75 mb-2"}),
-                               label="")
+        attrs={"placeholder": "Nom d'utilisateur", "class": "form-control w-75 mb-2"}), label="")
     password = forms.CharField(max_length=50, widget=forms.PasswordInput(
-        attrs={"placeholder": "Mot de passe", "class": "form-control w-75"}),
-                               label="")
+        attrs={"placeholder": "Mot de passe", "class": "form-control w-75"}), label="")
 
 
 class SignForm(UserCreationForm):
--- src/core/views.py
+++ src/core/views.py
@@ -1,10 +1,9 @@
-from django.contrib import messages
-
 from core.forms import LoginForm, SignForm
 from django.contrib.auth import authenticate, logout, login
 from django.shortcuts import redirect, render
 from django.views.generic import View
-
+from django.db.models import Value, CharField
+from itertools import chain
 from ticketing.models import Review, Ticket
 
 
@@ -56,13 +55,6 @@ class Logout(View):
         return redirect('login')
 
 
-class FluxPage(View):
-    template = "flux.html"
-
-    def get(self, request):
-        return render(request, self.template)
-
-
 class PostPage(View):
     template = "posts.html"
 
@@ -70,3 +62,13 @@ class PostPage(View):
         reviews = Review.objects.all()
         tickets = Ticket.objects.all()
         return render(request, self.template, context={"tickets": tickets, "reviews": reviews})
+
+
+class FluxPage(View):
+    template = "flux.html"
+
+    def get(self, request):
+        reviews = Review.objects.annotate(content_type=Value("REVIEW", CharField()))
+        tickets = Ticket.objects.annotate(content_type=Value("TICKET", CharField()))
+        posts = sorted(chain(reviews, tickets), key=lambda post: post.time_created, reverse=True)
+        return render(request, self.template, context={'posts': posts, 'tickets': tickets})
